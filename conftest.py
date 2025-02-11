55375fb8ee22c2b3cb4ed4223d31670b60919921
vsnegovik <snegovik.dev@gmail.com>
2025-02-12 00:54:58 +0500

    feat: add keycloack auth

--- conftest.py
+++ conftest.py
@@ -1,14 +1,17 @@
+import os
 import pytest
-from playwright.sync_api import sync_playwright
+import requests
 
+from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
 def browser():
     """Инициализация браузера Playwright перед тестами"""
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         yield browser
         browser.close()
+
 
 @pytest.fixture(scope="session")
 def page(browser):
@@ -20,18 +23,34 @@ def page(browser):
 
 @pytest.fixture(scope="function")
 def authenticated_page():
-    username = os.getenv("APP_USERNAME")
-    password = os.getenv("APP_PASSWORD")
-
-    assert username and password, "Ошибка: логин и пароль не заданы!"
+    """Логинимся в Keycloak через браузер перед каждым тестом"""
+    username = os.getenv("KEYCLOAK_USERNAME")
+    password = os.getenv("KEYCLOAK_PASSWORD")
 
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
 
+        # Переход на страницу логина Keycloak
+        page.goto("https://keycloak.example.com/auth/realms/myrealm/protocol/openid-connect/auth?client_id=myclient&response_type=code")
+
+        # Ввод логина и пароля
+        page.fill("#username", username)
+        page.fill("#password", password)
+        page.click("#kc-login")
+
+        page.wait_for_load_state("networkidle")
+
+        yield page  # Возвращаем авторизованную страницу
+
+        username = os.getenv("APP_USERNAME")
+        password = os.getenv("APP_PASSWORD")
+
+        assert username and password, "Ошибка: логин и пароль не заданы!"
+        
         # Авторизация
-        page.goto("https://wms.local:8585/sign-in")
+        page.goto("https://wms-frontend-core-release-dev.dev.cluster.kznexpess.com/sign-in")
         page.fill("[data-test-id='input__phone']", username)
         page.fill("[data-test-id='input__password']", password)
         page.click("[data-test-id='button__signIn']")
--- tests/ui/test_access.py
+++ tests/ui/test_access.py
@@ -2,6 +2,6 @@ import pytest
 
 @pytest.mark.ui
 def test_login(page):
-    page.goto("https://wms.local:8585/delivery-point")
+    page.goto("https://wms-frontend-core-release-dev.dev.cluster.kznexpess.com/delivery-point/route-sheets")
     page.wait_for_load_state("networkidle")  # Ждём загрузки страницы
     assert "Приёмка" in page.title()
